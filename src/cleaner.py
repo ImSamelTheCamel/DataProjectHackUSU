c83c188ae50eb70b8e7ab06bc3e6d44db090062a
hofhijus000 <161897481+hofhijus000@users.noreply.github.com>
2024-03-01 17:28:09 -0700

    Update cleaner.py

--- src/cleaner.py
+++ src/cleaner.py
@@ -1,1 +1,11 @@
-cleaner
+import pandas as pd
+
+df = pd.read_csv('../nba_2022-23_all_stats_with_salary.csv')
+print(df.head())
+
+df = df.drop(['Unnamed: 0', 'Position', 'Age', 'Team', 'GS', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P', '2PA', '2P%', 'FT', 'FTA', 'ORB', 'DRB', 'PF', 'Total Minutes', 'PER', '3PAr', 'FTr', 'ORB%', 'DRB%', 'TRB%', 'AST%', 'STL%', 'BLK%', 'TOV%', 'USG%', 'OWS', 'DWS', 'WS', 'WS/48', 'OBPM', 'DBPM', 'BPM', 'VORP'], axis=1)
+print(df)
+
+df = df.replace(None, 0)
+
+
